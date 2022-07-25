a9adc4526aaa904eb65cf51c22ee2efca0990724
Ubuntu <ubuntu@ip-172-31-9-121.us-east-2.compute.internal>
2022-07-25 03:36:13 +0000

    I want to play it takes 2

--- heatmap_gen.m
+++ heatmap_gen.m
@@ -2,7 +2,7 @@ clear all;
 clc;
 %% finger image data extract
 grid = [100 100];
-biomatrix3 = '20211014_Wenhan_finger_1.avi';
+biomatrix3 = 'input.avi';
 obj = VideoReader(biomatrix3);
 h = floor(linspace(0, obj.Height, grid(1)+1));
 h = diff(h);
--- something.py
+++ something.py
@@ -3,7 +3,7 @@ import matlab.engine
 
 def cool():
     eng = matlab.engine.start_matlab()
-    eng.heatmap_gen1(nargout=0)
+    eng.heatmap_gen(nargout=0)
     eng.quit()
 
 
@@ -11,3 +11,10 @@ def hot():
     eng = matlab.engine.start_matlab()
     eng.Hemoglobin_quantification(nargout=0)
     eng.quit()
+
+def grey():
+    eng = matlab.engine.start_matlab()
+    eng.grey(nargout=0)
+    eng.quit()
+
+grey()
